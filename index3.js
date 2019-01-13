7e771d4d126105221752fad2510c13ea01986c6b
XueSong <dxxzst@gmail.com>
2019-01-13 23:26:23 +0800

    update

--- index3.js
+++ index3.js
@@ -1,6 +1,19 @@
 const webdriver = require('selenium-webdriver');
 
-let driver = new webdriver.Builder().forBrowser('firefox').build();
-driver.get('http://www.baidu.com');
+let driver = new webdriver.Builder().forBrowser('safari').build();
+driver.get('https://free-ss.site/');
 
-driver.takeScreenshot();
+driver.sleep(15000);//毫秒
+
+driver.executeScript(`
+    $('table').map(function () {
+        return {
+            id:this.id,
+            height:$(this).height(),
+            data:$(this).DataTable().data()
+        }
+    })
+`).then(function(obj){
+    //obj即为返回值
+    console.log(obj);
+});
