4a3c59e5ddb243cdab35a9809a90b7c68825af6c
XueSong <song.xue@seek-data.com>
2019-01-16 09:58:41 +0800

    基本测试完成

--- index4.js
+++ index4.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 const url = require("url");
 const querystring = require("querystring");
-const request = require('request');
+const axios = require('axios');
 
 // puppeteer中文API https://zhaoqize.github.io/puppeteer-api-zh_CN/#/
 (async () => {
@@ -12,6 +12,12 @@ const request = require('request');
     await page.goto('https://www.ssrtool.com/tool/free_ssr');
     //await page.waitFor(5000);
 
+    // let res = await page.on('response', response => {
+    //     let message = JSON.stringify(response.text());
+    //     return Promise.resolve(message);
+    // });
+    // console.log(res);
+
     const subscriptionUrl = await page.evaluate(() => {
         return $('.mdui-textfield-input').val();
     });
@@ -19,11 +25,19 @@ const request = require('request');
     let query = url.parse(subscriptionUrl).query;
     let params = querystring.parse(query);
 
-    let getUrl = `https://www.ssrtool.com/tool/api/free_ssr?key=${params.key}&token=${new Date().getTime()}&page=1&limit=90`;
+    const getUrl = `https://www.ssrtool.com/tool/api/free_ssr?key=${params.key}&token=${new Date().getTime()}&page=1&limit=90`;
     console.log(getUrl);
 
-    let jsonData = request.pipe(fs.createWriteStream('temp.json'));
-    console.log(jsonData);
+    const dataList = await page.evaluate(() => {
+        return layui.table.cache.data;
+    });
+    console.log(dataList);
+
+    // axios.get(getUrl).then(function (response) {
+    //     console.log(response);
+    // }).catch(function (error) {
+    //     console.log(error);
+    // });
 
     browser.close();
 })();
